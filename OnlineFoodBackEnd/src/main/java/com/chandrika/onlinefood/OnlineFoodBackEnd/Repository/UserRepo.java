c68dd6bab42026956a47313e2d5448702de0d358
potnuruchandrika <162982661+potnuruchandrika@users.noreply.github.com>
2024-07-09 15:27:04 +0545

    Add files via upload

--- OnlineFoodBackEnd/src/main/java/com/chandrika/onlinefood/OnlineFoodBackEnd/Repository/UserRepo.java
+++ OnlineFoodBackEnd/src/main/java/com/chandrika/onlinefood/OnlineFoodBackEnd/Repository/UserRepo.java
@@ -1,5 +1,7 @@
 package com.chandrika.onlinefood.OnlineFoodBackEnd.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.chandrika.onlinefood.OnlineFoodBackEnd.Entity.User;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer>{
+
+	Optional<User> findByPhoneNo(String phoneNo);
+	
 	
 }
